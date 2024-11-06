48557d54fa1467c9a55b278b5caea9779e8bf2d8
Osmar Borges <osmar.borges@grupointegrado.br>
2024-11-05 23:31:57 -0300

    refactor(entrypoint): modificar script para criar tabelas antes de iniciar a aplicação Flask

--- docker-project/entrypoint.sh
+++ docker-project/entrypoint.sh
@@ -6,5 +6,9 @@ while ! nc -z mariadb_container 3306; do
     sleep 5
 done
 
-echo "MariaDB disponível. Iniciando a aplicação Flask."
+echo "MariaDB disponível. Iniciando a criação de tabelas."
+python -c "from app import db; db.create_all()"
+echo "Tabelas criadas com sucesso."
+
+echo "Iniciando a aplicação Flask."
 exec flask run --host=0.0.0.0 --port=5000
